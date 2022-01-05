79973c08051557b1975a4b820c7fabd23f5c1692
Jarmo Lindström <yaramodesu@gmail.com>
2022-01-05 15:33:02 +0200

    navbar paranneltu logiikalta

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import { GiSewingNeedle } from 'react-icons/gi';
 
 import Image from 'next/image';
@@ -7,77 +7,62 @@ import styled from '../styles/navbar.module.css';
 
 export default function Navbar() {
 	const [ toggleMenu, setToggleMenu ] = useState(false);
-	const [ screenWidth, setScreenWidth ] = useState(window.innerWidth);
-
-	const toggleNav = () => {
-		setToggleMenu(!toggleMenu);
-	};
-
-	useEffect(() => {
-		const chanceWidth = () => {
-			setScreenWidth(window.innerWidth);
-		};
-		window.addEventListener('resize', chanceWidth);
-
-		return () => {
-			window.removeEventListener('resize', chanceWidth);
-		};
-	}, []);
 
 	return (
 		<nav className={styled.navbar}>
 			<div className={styled.brand}>
-				<Image src="/images/logo/logosvg.svg" width={40} height={40} alt="Jantskun lanka" />
-				<span>Janskun lanka ja ompelupaja</span>
+				<div className={styled.logo}>
+					<Image src="/images/logo/logosvg.svg" width={40} height={40} alt="Jantskun lanka" />
+					<span>Janskun lanka ja ompelupaja</span>
+				</div>
+				<button className={styled.btn} onClick={() => setToggleMenu(!toggleMenu)}>
+					<GiSewingNeedle className={styled.icon} />
+				</button>
 			</div>
-			{(toggleMenu || screenWidth > 1199) && (
+			<div className={`${toggleMenu ? styled.esille : styled.piilossa} ${styled.links}`}>
 				<ul className={styled.navbarlist}>
-					<li>
+					<li className={styled.li}>
 						<Link href="/">
-							<a className={styled.navlink}>Etusivu</a>
+							<a className={styled.a}>Etusivu</a>
 						</Link>
 					</li>
-					<li>
+					<li className={styled.li}>
 						<Link href="/langat">
-							<a className={styled.navlink}>Langat</a>
+							<a className={styled.a}>Langat</a>
 						</Link>
 					</li>
-					<li>
+					<li className={styled.li}>
 						<Link href="/muuttuotteet">
-							<a className={styled.navlink}>Muut tuotteet</a>
+							<a className={styled.a}>Muut tuotteet</a>
 						</Link>
 					</li>
-					<li>
+					<li className={styled.li}>
 						<Link href="/ompelupalvelut">
-							<a className={styled.navlink}>Ompelupalvelut</a>
+							<a className={styled.a}>Ompelupalvelut</a>
 						</Link>
 					</li>
-					<li>
+					<li className={styled.li}>
 						<Link href="/pesulapalvelut">
-							<a className={styled.navlink}>Pesulapalvelut</a>
+							<a className={styled.a}>Pesulapalvelut</a>
 						</Link>
 					</li>
-					<li>
+					<li className={styled.li}>
 						<Link href="/esittely">
-							<a className={styled.navlink}>Esittely</a>
+							<a className={styled.a}>Esittely</a>
 						</Link>
 					</li>
-					<li>
+					<li className={styled.li}>
 						<Link href="/verkkokauppa">
-							<a className={styled.navlink}>Verkkokauppa</a>
+							<a className={styled.a}>Verkkokauppa</a>
 						</Link>
 					</li>
-					<li className={styled.link}>
+					<li className={styled.li}>
 						<Link href="/otayhteytta">
-							<a className={styled.navlink}>Ota yhteyttä</a>
+							<a className={styled.a}>Ota yhteyttä</a>
 						</Link>
 					</li>
 				</ul>
-			)}
-
-			<button className={styled.btn} onClick={toggleNav}>
-				<GiSewingNeedle className={styled.icon} />
-			</button>
+			</div>
 		</nav>
 	);
 }
