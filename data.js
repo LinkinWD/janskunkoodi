0a257f2a05086aa1d7789799cd2bbfe0aa4da7e0
Jarmo Lindström <yaramodesu@gmail.com>
2022-01-11 09:42:30 +0200

    data palautettu palveluntarjojien osalta

---
+++ data.js
@@ -0,0 +1,26 @@
+export const palvelutJaTuotteet = [
+	{
+		id: 1,
+		title: 'Langat',
+		img: '/images/muut/langat.jpg',
+		link: '/langat'
+	},
+	{
+		id: 2,
+		title: 'Muut tuotteet',
+		img: '/images/muut/langat.jpg',
+		link: '/muuttuotteet'
+	},
+	{
+		id: 3,
+		title: 'Ompelupalvelut',
+		img: '/images/muut/ompelu.jfif',
+		link: '/ompelupalvelut'
+	},
+	{
+		id: 1,
+		title: 'Pesulapalvelut',
+		img: '/images/muut/vista.jpg',
+		link: '/pesulapalvelut'
+	}
+];
