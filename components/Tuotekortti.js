9a1e81027873b892589f5c4a5ea7bd1076d5ae25
Jarmo Lindström <yaramodesu@gmail.com>
2022-01-14 11:59:34 +0200

    valmistaja lisätty tuotekorttiin

--- components/Tuotekortti.js
+++ components/Tuotekortti.js
@@ -2,12 +2,13 @@ import Image from 'next/image';
 import styled from '../styles/tuotekortti.module.css';
 
 export default function Tuotekortti({ tuote }) {
-	const { nimi, alaluokka, selostus, puikkosuositus, kuva, luokka } = tuote.fields;
+	const { nimi, alaluokka, selostus, puikkosuositus, kuva, luokka, valmistaja } = tuote.fields;
 	return (
 		<article className={styled.card}>
 			<h3 className={styled.title}>{nimi}</h3>
 			<Image src={`https:${kuva.fields.file.url}`} width={250} height={200} alt="lanka" />
 			<p className={styled.luokka}>Luokka: {alaluokka}</p>
+			<p>Valmistaja: {valmistaja}</p>
 			<p className={styled.selostus}>{selostus}</p>
 			{luokka ? <p>Puikkosuositus: {puikkosuositus}</p> : ''}
 		</article>
