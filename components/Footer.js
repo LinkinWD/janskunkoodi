2c4f1d1cf29aee05064305eb0dbaa32b460ccdef
Jarmo Lindström <yaramodesu@gmail.com>
2022-01-16 13:27:36 +0200

    contentful ja tule käymään asetelty

--- components/Footer.js
+++ components/Footer.js
@@ -5,28 +5,33 @@ import Link from 'next/link';
 export default function Footer() {
 	return (
 		<footer className={styled.footer}>
-			<div className={styled.div}>
-				<p>Janskun Lanka ja Ompelupaja</p>
-				<div className={styled.imagediv}>
-					<Link href="https://www.facebook.com/Janskunlanka">
-						<Image src="/images/icons/fb_icon.png" height={40} width={40} />
-					</Link>
-					<Link href={'https://www.instagram.com/janskunlanka/?hl=fi'}>
-						<Image src="/images/icons/insta_icon.png" height={40} width={40} />
-					</Link>
+			<div className={styled.firma}>
+				<div className={styled.div}>
+					<p>Janskun Lanka ja Ompelupaja</p>
+					<div className={styled.imagediv}>
+						<Link href="https://www.facebook.com/Janskunlanka">
+							<Image src="/images/icons/fb_icon.png" height={40} width={40} />
+						</Link>
+						<Link href={'https://www.instagram.com/janskunlanka/?hl=fi'}>
+							<Image src="/images/icons/insta_icon.png" height={40} width={40} />
+						</Link>
+					</div>
+				</div>
+				<div className={styled.div}>
+					<p>Kauppakatu 2, 30100 Forssa</p>
+					<p>Avoinna ma-pe 9-17, la 9-14</p>
+				</div>
+
+				<div className={styled.div}>
+					<p>janskunompelupaja@gmail.com</p>
+					<p>Puhelin: 050 361 5575</p>
 				</div>
 			</div>
-			<div className={styled.div}>
-				<p>Kauppakatu 2, 30100 Forssa</p>
-				<p>Avoinna ma-pe 9-17, la 9-14</p>
+			<div className={styled.contentful}>
+				<p>Powered by</p>
 				<Link href={'https://www.contentful.com'}>
-					<Image src={'/images/logo/contentful.png'} width={200} height={40} alt="Powered by Contentful" />
+					<Image src={'/images/logo/contentful.png'} width={150} height={30} alt="Powered by Contentful" />
 				</Link>
-			</div>
-
-			<div className={styled.div}>
-				<p>janskunompelupaja@gmail.com</p>
-				<p>Puhelin: 050 361 5575</p>
 			</div>
 		</footer>
 	);
--- pages/otayhteytta.js
+++ pages/otayhteytta.js
@@ -11,14 +11,10 @@ export default function otayhteytta() {
 			</Head>
 			<Otsikko otsikko={'Tule käymään'} />
 			<div className={styled.container}>
-				<div className={styled.image}>
-					<Image src={'/images/muut/maalaus.jpg'} alt="taustakuva" width={1800} height={700} />
-				</div>
 				<div className="kartta">
 					<iframe
+						className={styled.iframe}
 						src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d486.4171661139452!2d23.6216377!3d60.8132051!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x468e9cb607b47fb3%3A0x244c2a68e33e6cf9!2sKauppakatu%202%2C%2030100%20Forssa!5e0!3m2!1sfi!2sfi!4v1642324539164!5m2!1sfi!2sfi"
-						width={600}
-						height={450}
 						allowfullscreen=""
 						loading="lazy"
 					/>
