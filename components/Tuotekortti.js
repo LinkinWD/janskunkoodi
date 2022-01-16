0c76765f43e0a468246a743b424283622a97ff34
Jarmo Lindström <yaramodesu@gmail.com>
2022-01-16 12:52:42 +0200

    puikonkoko lisätty

--- components/Tuotekortti.js
+++ components/Tuotekortti.js
@@ -2,7 +2,8 @@ import Image from 'next/image';
 import styled from '../styles/tuotekortti.module.css';
 
 export default function Tuotekortti({ tuote }) {
-	const { nimi, alaluokka, selostus, puikkosuositus, kuva, luokka, valmistaja } = tuote.fields;
+	const { nimi, alaluokka, selostus, puikonkoko, kuva, luokka, valmistaja } = tuote.fields;
+
 	return (
 		<article className={styled.card}>
 			<h3 className={styled.title}>{nimi}</h3>
@@ -10,7 +11,7 @@ export default function Tuotekortti({ tuote }) {
 			<p className={styled.luokka}>Luokka: {alaluokka}</p>
 			<p>Valmistaja: {valmistaja}</p>
 			<p className={styled.selostus}>{selostus}</p>
-			{luokka ? <p>Puikkosuositus: {puikkosuositus}</p> : null}
+			<p>{luokka ? `Puikkosuositus: ${puikonkoko}` : null}</p>
 		</article>
 	);
 }
