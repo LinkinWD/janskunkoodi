9e6c1f248295ee3ddae7b4512e8b129118d3c2be
Jarmo Lindström <yaramodesu@gmail.com>
2022-02-09 07:51:54 +0200

    joku errori poistettu

--- pages/verkkokauppa/index.js
+++ pages/verkkokauppa/index.js
@@ -4,6 +4,16 @@ import Otsikko from '../../components/Otsikko';
 import Verkkokauppakortti from '../../components/Verkkokauppakortti';
 import VerkkokauppaLista from '../../components/VerkkokauppaLista';
 
+export const getServerSideProps = async () => {
+	const res = await fetch(`${process.env.SERVER_URL}/api/langat`);
+	const data = await res.json();
+	return {
+		props: {
+			langat: data
+		}
+	};
+};
+
 export default function verkkokauppa({ langat }) {
 	return (
 		<div className="kauppa">
@@ -15,12 +25,3 @@ export default function verkkokauppa({ langat }) {
 		</div>
 	);
 }
-export const getServerSideProps = async () => {
-	const res = await fetch(`${process.env.SERVER_URL}/api/langat`);
-	const data = await res.json();
-	return {
-		props: {
-			langat: data
-		}
-	};
-};
