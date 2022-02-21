dbe22de59d38c7b9c7834d4fd01cfce8982a4525
Jarmo Lindström <yaramodesu@gmail.com>
2022-02-21 08:03:52 +0200

    testi

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import styled from '../styles/404.module.css';
 
 export default function EiOle() {
 	return (
-		<div>
-			<h3>Error sivu, oops jotain meni vikaan ja tälläistä sivua ei voida näyttää</h3>
+		<div className={styled.div}>
+			<h3 className={styled.text}>Oops jotain meni vikaan ja tälläistä sivua ei voida näyttää</h3>
 		</div>
 	);
 }
